aabc127d83f7228d65262b53111903467a563061
lw99966 <126117937+lw99966@users.noreply.github.com>
2024-12-03 13:36:27 -0500

    Create Notification.js

---
+++ Notification.js
@@ -0,0 +1,14 @@
+class Notification {
+    constructor(message, date) {
+        this.message = message;
+        this.date = date;
+    }
+
+    getMessage() {
+        return this.message;
+    }
+
+    getDate() {
+        return this.date;
+    }
+}
